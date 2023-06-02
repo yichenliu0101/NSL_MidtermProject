2b5e164bf2e377e0f9ae458e93618a256a45df82
RiOuO <128416852+RiOuO@users.noreply.github.com>
2023-06-02 11:02:06 +0800

    0602 new

--- NSL/ISpanNSL/ISpanNSL/FrmCouponCreateAndEdit.cs
+++ NSL/ISpanNSL/ISpanNSL/FrmCouponCreateAndEdit.cs
@@ -10,31 +10,11 @@ using System.Windows.Forms;
 
 namespace ISpanNSL
 {
-<<<<<<< HEAD:NSL/ISpanNSL/ISpanNSL/FrmCouponCreateAndEdit.cs
 	public partial class FrmCouponCreateAndEdit : Form
 	{
 		public FrmCouponCreateAndEdit()
 		{
 			InitializeComponent();
-=======
-    public partial class frmMember : Form
-    {
-        public frmMember()
-        {
-            InitializeComponent();
-        }
-
-		private void pictureBoxAddAdmin_Click(object sender, EventArgs e)
-		{
-            var form = new FrmAddAdmin();
-            form.ShowDialog();
-		}
-
-		private void labAddAdmin_Click(object sender, EventArgs e)
-		{
-			var form = new FrmAddAdmin();
-			form.ShowDialog();
->>>>>>> b854668f68e5471cae471b9c45563366e874a215:pjNSL/pjNSL/frmMember.cs
 		}
 	}
 }
